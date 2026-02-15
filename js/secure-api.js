b22e651cd4670d9162b34c9c07397eeba1974cc3
sonex-pro <cbj.uk.design@gmail.com>
2026-02-15 18:50:36 +0000

    Fix CORS issue: use relative path for Netlify functions

--- js/secure-api.js
+++ js/secure-api.js
@@ -4,7 +4,7 @@
  */
 
 // Base URL for Netlify functions
-const API_BASE_URL = 'https://tournament2.netlify.app/.netlify/functions';
+const API_BASE_URL = '/.netlify/functions';
 
 /**
  * Create a secure tournament entry and Stripe checkout session
